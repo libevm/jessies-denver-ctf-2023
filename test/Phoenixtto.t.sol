38c460cbcc49279b1b8fb45e4210052ecf525be6
rotcivegaf <victorfage@gmail.com>
2023-03-02 21:04:09 -0700

    up test

---
+++ test/Phoenixtto.t.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "forge-std/Test.sol";
+import "openzeppelin-contracts/contracts/proxy/Clones.sol";
+import {Phoenixtto} from "../src/Phoenixtto.sol";
+
+contract PhoenixttoTest is Test {
+    Laboratory lab;
+
+    function setUp() public {
+        lab = new Laboratory();
+    }
+
+    function testhack() public {
+        address hacker = makeAddr("hacker");
+        vm.startPrank(hacker);
+
+        // do stuff
+
+        assertEq(lab.isCaught(), true, "ser hacker, drain the contract to win");
+    }
+}
