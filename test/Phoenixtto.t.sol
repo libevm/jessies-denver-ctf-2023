4e83a6f97ca82f10f99c85636b50402565e12c26
eugenioclrc <eugenioclrc@gmail.com>
2023-03-03 01:12:08 -0300

    fix phoenix

--- test/Phoenixtto.t.sol
+++ test/Phoenixtto.t.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 import "forge-std/Test.sol";
 import "openzeppelin-contracts/contracts/proxy/Clones.sol";
-import {Phoenixtto} from "../src/Phoenixtto.sol";
+import {Phoenixtto, Laboratory} from "../src/Phoenixtto.sol";
 
 contract PhoenixttoTest is Test {
     Laboratory lab;
